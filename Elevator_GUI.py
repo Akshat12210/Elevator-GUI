b8857e568ae4e556feb5c12323f12e3e58aadffb
Akshat Modani <72137725+Akshat12210@users.noreply.github.com>
2021-12-16 14:01:39 +0530

    Update Elevator_GUI.py

--- Elevator_GUI.py
+++ Elevator_GUI.py
@@ -1,7 +1,7 @@
 import time
 import tkinter as tk
 from tkinter import *
-
+import tkinter.messagebox
 
 #############Code#####################################################
 window = Tk()
@@ -10,29 +10,29 @@ window.geometry("500x600")
 window.configure(background="black")
 Title = Label (window, text="Elevator", bg="black", fg="white", font="Arial 14 bold")
 Title.pack()
-canvas = Canvas(window, width=500, height=500)
+canvas = Canvas(window, width=500,height=500)
 canvas.pack()
-elevator = canvas.create_rectangle(60, 500, 80, 440, fill="grey")
+elevator = canvas.create_rectangle(60, 480, 80, 420, fill="grey")
 ###########################All the boxes and lines##########################################
 
-canvas.create_line(60,80,60,500, fill="black")
-canvas.create_line(80,80,80,140, fill="black")
-canvas.create_line(80,200,80,260, fill="black")
-canvas.create_line(80,320,80,380, fill="black")
-canvas.create_line(80,440,80,500,fill='black')
-canvas.create_line(480,80,480,500, fill="black")
-canvas.create_line(60,80,480,80)
-canvas.create_line(60,140,480,140)
-canvas.create_line(60,200,480,200)
-canvas.create_line(60,260,480,260)
-canvas.create_line(60,320,480,320)
-canvas.create_line(60,380,480,380)
-canvas.create_line(60,440,480,440)
-canvas.create_line(60,500,480,500)
-canvas.create_text(70, 470, fill="black", font="none 10 bold", text="G")
-canvas.create_text(70, 350, fill="black", font="none 10 bold", text="1")
-canvas.create_text(70, 230, fill="black", font="none 10 bold", text="2")
-canvas.create_text(70, 110, fill="black", font="none 10 bold", text="3")
+canvas.create_line(60,60,60,480, fill="black")
+canvas.create_line(80,60,80,120, fill="black")
+canvas.create_line(80,180,80,240, fill="black")
+canvas.create_line(80,300,80,360, fill="black")
+canvas.create_line(80,420,80,480,fill='black')
+canvas.create_line(480,60,480,480, fill="black")
+canvas.create_line(60,60,480,60)
+canvas.create_line(60,120,480,120)
+canvas.create_line(60,180,480,180)
+canvas.create_line(60,240,480,240)
+canvas.create_line(60,300,480,300)
+canvas.create_line(60,360,480,360)
+canvas.create_line(60,420,480,420)
+canvas.create_line(60,480,480,480)
+canvas.create_text(70, 450, fill="black", font="Arial 10 bold", text="G")
+canvas.create_text(70, 330, fill="black", font="Arial 10 bold", text="1")
+canvas.create_text(70, 210, fill="black", font="Arial 10 bold", text="2")
+canvas.create_text(70, 90, fill="black", font="Arial 10 bold", text="3")
 
 #################################################################################
 CURRENT_FLOOR = 0
@@ -71,10 +71,14 @@ def move(floor):
 def UP():
     if CURRENT_FLOOR<3:
         move(CURRENT_FLOOR+1)
+    else:
+        tkinter.messagebox.showinfo('Alert','You are on the Top Floor')
         
 def DOWN():
     if CURRENT_FLOOR>0:
         move(CURRENT_FLOOR-1)
+    else:
+         tkinter.messagebox.showinfo('Alert','You are on the Bottom Floor')
 
 ####################################################################################################################
 
@@ -85,17 +89,17 @@ blank1.pack(side=TOP)
 new_floor = 0
 
 while True:
-    gf = Button (window, text="G", fg="green", activebackground="white", command=lambda: move(0))
-    gf.place(x=132,y=560)
-    floor1 = Button (window, text="1", bg="black", fg="green", activebackground="white", command=lambda: move(1))
-    floor1.place(x=200,y=560)
-    floor2 = Button (window, text="2", bg="black", fg="green", activebackground="white", command=lambda: move(2))
-    floor2.place(x=260,y=560)
-    floor3 = Button (window, text="3", bg="black", fg="green", activebackground="white", command=lambda: move(3))
-    floor3.place(x=320,y=560)
-    up=Button(window,bg="cyan",width=3,height=2,text="Up",command=lambda: UP())
-    up.place(x=20,y=250)
-    down=Button(window,bg="cyan",width=4,height=2,text="Down",command=lambda: DOWN())
-    down.place(x=20,y=300)
+    gf = Button (window, text="G",font="Arial 10 bold", width=4,height=1,fg="green", activebackground="white", command=lambda: move(0))
+    gf.place(x=132,y=565)
+    floor1 = Button (window, text="1",font="Arial 10 bold",width=4,height=1, bg="black", fg="green", activebackground="white", command=lambda: move(1))
+    floor1.place(x=200,y=565)
+    floor2 = Button (window, text="2",font="Arial 10 bold",width=4,height=1, bg="black", fg="green", activebackground="white", command=lambda: move(2))
+    floor2.place(x=260,y=565)
+    floor3 = Button (window, text="3",font="Arial 10 bold",width=4,height=1, bg="black", fg="green", activebackground="white", command=lambda: move(3))
+    floor3.place(x=320,y=565)
+    up=Button(window,bg="black",fg="white",font="Arial 9 bold",width=5,height=2,text="Up",command=lambda: UP())
+    up.place(x=10,y=250)
+    down=Button(window,bg="black",fg="white",font="ARial 9 bold",width=5,height=2,text="Down",command=lambda: DOWN())
+    down.place(x=10,y=300)
     break
 mainloop()
